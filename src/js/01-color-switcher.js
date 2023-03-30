73c1bb36089bf9f291be219c4d6bf166b73b96b4
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-03-30 18:28:56 +0300

    1212

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,23 @@
+"use strict";
+
+
 const startBtnEl = document.querySelector("[data-start]");
 const stopBtnEl = document.querySelector("[data-stop]");
 const bodyEl = document.querySelector("body");
 
-startBtnEl.addEventListener("click", (event) => {
-    let timerId = setInterval(() => {
-        bodyEl.style.backgroundColor = getRandomHexColor();
+let timerId;
 
-        if (event.target) {
-            startBtnEl.setAttribute("disabled", "");
-            stopBtnEl.removeAttribute("disabled");
-        }
-    }, 1000);
-});
+    startBtnEl.addEventListener("click", (event) => {
+         timerId = setInterval(() => {
+            bodyEl.style.backgroundColor = getRandomHexColor();
+
+            if (event.target) {
+                startBtnEl.setAttribute("disabled", "");
+                stopBtnEl.removeAttribute("disabled");
+            }
+        }, 1000);
+    });
+    
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
