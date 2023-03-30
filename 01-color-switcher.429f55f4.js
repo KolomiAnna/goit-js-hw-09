1ab20ced4960137bd1358313ee715a87b3356c83
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-03-30 13:57:29 +0000

    Deploying to gh-pages from @ KolomiAnna/goit-js-hw-09@e4fd313ebd66a64f715c4658065625b719fad8ac 🚀

---
+++ 01-color-switcher.429f55f4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(a){timerId=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),a.target&&(t.setAttribute("disabled",""),e.removeAttribute("disabled"))}),1e3)})),e.addEventListener("click",(function(r){clearInterval(timerId),r.target&&(t.removeAttribute("disabled"),e.setAttribute("disabled",""))}))}();
+//# sourceMappingURL=01-color-switcher.429f55f4.js.map
---
+++ 02-timer.e0c62bdb.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=02-timer.e0c62bdb.js.map
